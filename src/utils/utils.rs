96764e5d194e3dccff19e5a570f0335cdce0c656
CPerezz <c.perezbaro@gmail.com>
2018-11-22 13:33:06 +0100

    Removing excessive mod

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,5 +1,4 @@
-pub mod utils {
-    pub fn string_to_static_str(s: String) -> &'static str {
-        Box::leak(s.into_boxed_str())
-    }
+
+pub fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
 }
